a2f4a39b68aadbfdbce834beb5474a48e00ab18f
kovalyovdsMac <kovalyovds@gmail.com>
2022-04-12 17:56:22 +0300

    hw_02

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import {useState} from "react";
+
 import css from './App.module.css';
+import {postService} from "./services";
 import Users from "./components/Users/Users";
 import UserDetails from "./components/UserDetails/UserDetails";
 import Posts from "./components/Posts/Posts";
-import {useState} from "react";
-import {postService} from "./services";
 
 const App = () => {
     const [userDetail, setUserDetails] = useState(null);
