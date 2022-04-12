d53ac64505efc2cca7597f79a2c8eedaec7d618d
kovalyovdsMac <kovalyovds@gmail.com>
2022-04-12 15:05:46 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import './App.css';
-import { Comments, Posts, Users } from './components';
-
-export const App = () => {
+function App() {
     return (
         <div>
-            <div className={'users_posts'}>
-                <Users/>
-                <Posts/>
-            </div>
-            <Comments/>
+
         </div>
     );
 }
+
+export default App;
