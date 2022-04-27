683f13493686e9bc2947b862b4179f08c8ceb522
kovalyovdsMac <kovalyovds@gmail.com>
2022-04-27 12:15:46 +0300

    hw_05_1

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export * from './Comment/comment';
-export * from './Comments/comments';
-export * from './Post/post';
-export * from './Posts/posts';
-export * from './User/user';
-export * from './Users/users';
--- src/services/index.js
+++
@@ -1,3 +0,0 @@
-export * from './comment.services';
-export * from './posts.services';
-export * from './users.services';
